e871cae990c0fded96a3dc844ad01df3bae80238
Shrishyle Pandit <shrishyle@yahoo.com>
2023-08-30 19:07:40 +0530

    Partially completed Form Facility

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ for (let i = 0; i < rows.length; i++) {
   let newRow = createMeetingDetailRow();
   rows[i].parentElement.insertBefore(newRow, rows[i].nextElementSibling);
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".chips");
+  var instances = M.Chips.init(elems, {
+    data: ["Shrishyle"],
+  });
+});
+
+const Recipient = class {
+  constructor(name, description, term) {
+    this.name = name;
+    this.description = description;
+    this.term = term;
+  }
+};
