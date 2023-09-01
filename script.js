16923798e8ada8859f072aef5ca444f602ec0c1b
Shrishyle Pandit <64643330+shrishyle@users.noreply.github.com>
2023-09-02 00:34:27 +0530

    Name Rendering Issue Resolved
    The names were not being rendered properly. Also the data from the form was not being rendered properly in the text. This issue now stands resolved.

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const RecipientClass = class {
-  constructor(name, description = "", term = "short") {
+  constructor(name, title = "", status = "variable") {
     this.name = name;
-    this.description = description;
-    this.term = term;
+    this.title = title;
+    this.status = status;
   }
 };
+
+const database = class {
+  constructor(title, dbName, modifiedDB) {
+    this.title = title;
+    this.setdb = () => {
+      localStorage.setItem(dbName, JSON.stringify(modifiedDB));
+    };
+    this.getdb = () => {
+      let extractedDB = JSON.parse(localStorage.getItem(dbName));
+      return extractedDB;
+    };
+    this.setdb();
+    this.getdb();
+  }
+};
+
+let dbObject = [
+  { name: "Shri. Sateesh Kumar", title: "", status: "variable" },
+  { name: "Shri. Devesh Srivastava", title: "", status: "variable" },
+  { name: "Internal Audit Department", title: "", status: "fixed" },
+  { name: "Assistant General Manager", title: "Internal Audit Department", status: "fixed" },
+  { name: "Deputy General Manager", title: "Internal Audit Department", status: "fixed" },
+  { name: "General Manager", title: "Internal Audit Department", status: "fixed" },
+];
+
+let recipientList = new database("db2", "directorDB", dbObject);
 
 let dataClass = class {
   constructor() {
@@ -26,35 +52,38 @@ let dataClass = class {
       }
     };
 
-    //Method to add Director's name in storage
+    //Method to add Director's name in dbObject
     this.addNameInStorage = () => {
-      let addNameBtn = document.getElementById("add-name");
-      addNameBtn.addEventListener("click", () => {
-        let inputElem = document.getElementById("new-director-name");
-        let newDirectorName = inputElem.value;
-        let newRecipient = new RecipientClass(newDirectorName);
-        this.storage.push(newRecipient);
-        document.getElementById("new-director-name").value = "";
-        this.clearAllChips();
-        this.renderNameList();
+      let inputElem = document.getElementById("new-director-name");
+      inputElem.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+          let newDirectorName = inputElem.value;
+          let newRecipient = new RecipientClass(newDirectorName);
+          dbObject.push(newRecipient);
+          document.getElementById("new-director-name").value = "";
+          this.clearAllChips();
+          this.renderNameList();
+        }
       });
     };
 
     //Method to render Name List of Directors
     this.renderNameList = () => {
       this.clearAllChips();
-      this.storage.forEach((item) => {
-        let chipColumn = document.getElementById("chip-column");
-        let template = document.getElementById("chip-template");
-        template.content.querySelector("span").textContent = item.name;
-        let tempElement = template.content.cloneNode(true).children[0];
-        chipColumn.appendChild(tempElement);
+      dbObject.forEach((item) => {
+        if (item.status !== "fixed") {
+          let chipColumn = document.getElementById("chip-column");
+          let template = document.getElementById("chip-template");
+          template.content.querySelector("span").textContent = item.name;
+          let tempElement = template.content.cloneNode(true).children[0];
+          chipColumn.appendChild(tempElement);
+        }
       });
     };
 
     //Method to save Meeting Information
     this.saveMeetingInfoAndRenderTemplate = () => {
-      let saveMeetingInfoBtn = document.getElementById("save-meeting-info");
+      let saveMeetingInfoBtn = document.getElementById("render-labels");
       saveMeetingInfoBtn.addEventListener("click", () => {
         let meetingNoInputElem = document.getElementById("first_name");
         let meetingDateInputElem = document.getElementById("meeting_date");
@@ -63,6 +92,7 @@ let dataClass = class {
         this.meetingInfo.meetingNumber = meetingNo;
         this.meetingInfo.meetingDate = new Date(meetingDate);
         this.renderMeetingDetailsRows(this.injectMeetingInformation());
+        this.renderDirectorNamesInLabels();
       });
     };
 
@@ -123,7 +153,6 @@ let dataClass = class {
       // console.log("Line No. - 114", "temp - ", temp);
       let clone = temp.cloneNode(true);
       let rows = document.querySelectorAll(".attendee_name");
-      console.log(rows);
       let tr = document.createElement("tr");
       tr.appendChild(temp);
       tr.appendChild(clone);
@@ -132,6 +161,19 @@ let dataClass = class {
         rows[i].insertAdjacentElement("afterend", tr_clone);
       }
     };
+
+    //Function to render names of Directors
+    this.renderDirectorNamesInLabels = () => {
+      let nameDivs = document.getElementsByClassName("name");
+      let titleDivs = document.getElementsByClassName("title");
+      let i = 0;
+      while (i < dbObject.length) {
+        nameDivs[i].textContent = dbObject[i].name;
+        titleDivs[i].textContent = dbObject[i].title;
+        i++;
+      }
+    };
+
     this.renderNameList();
     this.addNameInStorage();
     this.saveMeetingInfoAndRenderTemplate();
