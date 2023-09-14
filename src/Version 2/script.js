d7fbc3abdc33f20f0076a3dc70d7b83bc3b97b49
Shrishyle Pandit <64643330+shrishyle@users.noreply.github.com>
2023-09-14 07:57:15 +0530

    worked on the project

--- src/Version 2/script.js
+++ src/Version 2/script.js
@@ -1,27 +1,46 @@
-let meeting_detail_component_template = document.getElementById("meeting-detail-component-template");
-let component = meeting_detail_component_template.content.cloneNode(true);
-let container = document.getElementById("container");
-container.appendChild(component);
+const app = class {
+  constructor() {
+    this.displayFirstComponent = () => {
+      let meeting_detail_component_template = document.getElementById("meeting-detail-component-template");
+      let component = meeting_detail_component_template.content.cloneNode(true);
+      let container = document.getElementById("container");
+      container.appendChild(component);
+    };
+    this.next_button_feature = () => {
+      let next_button = document.getElementById("next-button");
+      if (next_button) {
+        next_button.addEventListener("click", () => {
+          let container = document.getElementById("container");
 
-let next_button = document.getElementById("next-button");
-next_button.addEventListener("click", () => {
-  let meeting_number_input = document.getElementById("meeting-number");
-  let meeting_number = meeting_number_input.value;
-  let meeting_date_input = document.getElementById("meeting-date");
-  let meeting_date = meeting_date_input.value;
-  let toast_container_template = document.getElementById("toast-container-template");
-  let toast_container = toast_container_template.content.cloneNode(true);
+          let meeting_number_input = document.getElementById("meeting-number");
+          let meeting_date_input = document.getElementById("meeting-date");
+          let meeting_number = meeting_number_input.value;
+          let meeting_date = meeting_date_input.value;
 
-  recipientList.getdb().forEach((item) => {
-    let toast_component_template = document.getElementById("toast-component-template");
-    let toast_component = toast_component_template.content.cloneNode(true);
-    let toast_text_field = toast_component.querySelector("#toast-text");
-    toast_text_field.textContent = item.name;
-    toast_container.appendChild(toast_component);
-    console.log(toast_container);
-  });
-  container.appendChild(toast_container);
-});
+          let toast_container_template = document.getElementById("toast-container-template");
+          let toast_container = toast_container_template.content.cloneNode(true);
+
+          let sub_container = document.createElement("div");
+          sub_container.setAttribute("id", "sub-container");
+          sub_container.classList.add("sub-container");
+          sub_container.appendChild(toast_container);
+
+          recipientList.getdb().forEach((item) => {
+            let toast_component_template = document.getElementById("toast-component-template");
+            let toast_component = toast_component_template.content.cloneNode(true);
+            let toast_text_field = toast_component.querySelector("#toast-text");
+            toast_text_field.textContent = item.name;
+            toast_container.appendChild(toast_component);
+          });
+          container.appendChild(sub_container);
+        });
+      }
+    };
+    this.displayFirstComponent();
+  }
+};
+
+let App = new app();
 
 const RecipientClass = class {
   constructor(name, title = "", status = "variable") {
