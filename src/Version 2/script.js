a394ce1d6b99f651b3a1682aed47e50b111970c1
Shrishyle Pandit <shrishyle@yahoo.com>
2023-09-14 18:32:34 +0530

    Parially worked

--- src/Version 2/script.js
+++ src/Version 2/script.js
@@ -1,42 +1,89 @@
 const app = class {
   constructor() {
+    //Method to display First Component
     this.displayFirstComponent = () => {
       let meeting_detail_component_template = document.getElementById("meeting-detail-component-template");
       let component = meeting_detail_component_template.content.cloneNode(true);
       let container = document.getElementById("container");
       container.appendChild(component);
     };
+
+    //Method to run feature on clicking "Next" Button on First Component.
     this.next_button_feature = () => {
       let next_button = document.getElementById("next-button");
       if (next_button) {
         next_button.addEventListener("click", () => {
+          //Code for inserting the Second Component
+          let second_component_template = document.getElementById("second-component-template");
+          let second_component = second_component_template.content.cloneNode(true);
+
+          // Level 1
+          second_component.querySelector("#back-button").addEventListener("click", () => {
+            let sec_com = document.getElementById("second-component");
+            sec_com.remove();
+            let next_btn_template = document.getElementById("next-button-template");
+            let next_btn_div = next_btn_template.content.cloneNode(true);
+            // Level 2
+            next_btn_div.querySelector("button").addEventListener("click", () => {
+              let second_component_template = document.getElementById("second-component-template");
+              let second_component = second_component_template.content.cloneNode(true);
+              let container = document.getElementById("container");
+              container.appendChild(second_component);
+              let toast_container1 = document.getElementsByClassName("toast-container")[0];
+              recipientList.getdb().forEach((item) => {
+                let toast_component_template = document.getElementById("toast-component-template");
+                let toast_component = toast_component_template.content.cloneNode(true);
+                let toast_text_field = toast_component.querySelector("#toast-text");
+                toast_text_field.textContent = item.name;
+                toast_container1.appendChild(toast_component);
+              });
+            });
+            document.getElementsByClassName("bottom")[0].appendChild(next_btn_div);
+          });
+
           let container = document.getElementById("container");
+          container.appendChild(second_component);
 
-          let meeting_number_input = document.getElementById("meeting-number");
-          let meeting_date_input = document.getElementById("meeting-date");
-          let meeting_number = meeting_number_input.value;
-          let meeting_date = meeting_date_input.value;
-
-          let toast_container_template = document.getElementById("toast-container-template");
-          let toast_container = toast_container_template.content.cloneNode(true);
-
-          let sub_container = document.createElement("div");
-          sub_container.setAttribute("id", "sub-container");
-          sub_container.classList.add("sub-container");
-          sub_container.appendChild(toast_container);
-
+          //Code for populating the toast-container with Recepient Names.
+          let toast_container1 = document.getElementsByClassName("toast-container")[0];
           recipientList.getdb().forEach((item) => {
             let toast_component_template = document.getElementById("toast-component-template");
             let toast_component = toast_component_template.content.cloneNode(true);
             let toast_text_field = toast_component.querySelector("#toast-text");
             toast_text_field.textContent = item.name;
-            toast_container.appendChild(toast_component);
+            toast_container1.appendChild(toast_component);
           });
-          container.appendChild(sub_container);
+
+          //Code for removing the buttons
+          next_button.parentElement.remove();
+          let bottomDiv = document.getElementsByClassName("bottom");
+          for (let i = 0; i < bottomDiv.length; i++) {
+            bottomDiv[i].classList.remove("view-1");
+            bottomDiv[i].classList.add("view-2");
+          }
         });
       }
     };
+
+    this.button = () => {
+      let buttonElement = document.createElement("button");
+      buttonElement.addEventListener("click", (event) => {
+        this.buttonFunc(event.target.textContent);
+      });
+      return buttonElement;
+    };
+
+    this.buttonFunc = (identifier) => {
+      if (identifier === "Next") {
+        let second_component_template = document.getElementById("second-component-template");
+        let second_component = second_component_template.content.cloneNode(true);
+        // Incomplete...working here!!
+      }
+    };
+
+    //Method to run feature on clicking "Back" Button on Second Component.
     this.displayFirstComponent();
+    this.next_button_feature();
   }
 };
 
@@ -72,18 +119,18 @@ let dbObject = [
   {
     name: "Shri. Devesh Srivastava",
   },
-  //   {
-  //     name: "Internal Audit Department",
-  //   },
-  //   {
-  //     name: "Assistant General Manager",
-  //   },
-  //   {
-  //     name: "Deputy General Manager",
-  //   },
-  //   {
-  //     name: "General Manager",
-  //   },
+  {
+    name: "Internal Audit Department",
+  },
+  {
+    name: "Assistant General Manager",
+  },
+  {
+    name: "Deputy General Manager",
+  },
+  {
+    name: "General Manager",
+  },
 ];
 
 let recipientList = new database("db2", "directorDB", dbObject);
